6493ad002a14fc5c06f8d6aa7026b76847dbeb8e
SCAUZSJ <895104160@qq.com>
2019-07-12 15:31:16 +0800

    return 2 if num is 2

--- src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
@@ -6,14 +6,24 @@ import org.junit.jupiter.api.Test;
 public class FizzBuzzTest {
 
     @Test
-    public void hello_world_test() {
+    public void return_1_when_num_is_1() {
         //given
         FizzBuzz fizzBuzz = new FizzBuzz();
         int num = 1;
         //when
-        String result = fizzBuzz.convert(1);
+        String result = fizzBuzz.convert(num);
         //then
         Assertions.assertEquals("1",result);
-
     }
+    @Test
+    public void return_2_when_num_is_2() {
+        //given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int num = 2;
+        //when
+        String result = fizzBuzz.convert(num);
+        //then
+        Assertions.assertEquals("2",result);
+    }
+
 }
